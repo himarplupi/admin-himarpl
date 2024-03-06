80290f76004951a02f62796e763f8b411f6bf941
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-03-06 15:36:54 +0700

    feat: neon-serverless database schema

---
+++ src/server/db/index.ts
@@ -0,0 +1,19 @@
+import { neon } from "@neondatabase/serverless";
+import type { NeonQueryFunction } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "@/env";
+import * as schema from "./schema";
+
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: NeonQueryFunction<boolean, boolean> | undefined;
+};
+
+const conn = globalForDb.conn ?? neon(env.DATABASE_URL);
+
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn, { schema });
