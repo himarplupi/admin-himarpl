12360d5dcad33e85cc79b0fd1794bdb0d035d9d7
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-03-08 19:12:58 +0700

    feat: data-table ui

--- src/app/departments/page.tsx
+++ src/app/departments/page.tsx
@@ -1,7 +1,17 @@
-import { Table, TableBody, TableHeader } from "@/components/ui/table";
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "@/server/auth";
+import { DataTableDemo } from "./_components/data-table";
 
-export default function DepartmentsPage() {
+export default async function DepartmentsPage() {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return redirect("/login");
+  }
+
   return (
-    <main className="container min-h-screen flex-1 space-y-4 p-8 pt-12"></main>
+    <main className="container min-h-screen flex-1 space-y-4 p-8 pt-12">
+      <DataTableDemo />
+    </main>
   );
 }
