51c8454157ac8ad4ecdcc1adaf2609675290a474
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-03-08 17:22:09 +0700

    feat: table deps and components

---
+++ src/app/departments/page.tsx
@@ -0,0 +1,7 @@
+import { Table, TableBody, TableHeader } from "@/components/ui/table";
+
+export default function DepartmentsPage() {
+  return (
+    <main className="container min-h-screen flex-1 space-y-4 p-8 pt-12"></main>
+  );
+}
