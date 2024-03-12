a080cebc00b44721e07950a2d0bb1cbb63b28f93
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-03-12 10:02:30 +0700

    feat: Minion Pro Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { Montserrat as FontSans } from "next/font/google";
+import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Toaster } from "@/components/ui/sonner";
@@ -8,6 +9,52 @@ import { Toaster } from "@/components/ui/sonner";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+const fontSerif = localFont({
+  src: [
+    {
+      path: "./fonts/minion-pro/MinionPro-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-It.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-MediumIt.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-SemiBoldIt.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-BoldIt.otf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-serif",
 });
 
 export const metadata = {
@@ -27,6 +74,7 @@ export default function RootLayout({
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
+          fontSerif.variable,
         )}
       >
         <TRPCReactProvider>{children}</TRPCReactProvider>
