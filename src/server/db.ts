2c077c025c540e6e94cd6d5cdd872348a85f9a36
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2025-02-25 10:51:42 +0700

    chore: update deps

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,18 +1,26 @@
+import { env } from "@/env";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "@/env";
+const libsql = createClient({
+  url: `${env.DATABASE_URL}`,
+  authToken: `${env.TURSO_AUTH_TOKEN}`,
+});
 
-const createPrismaClient = () => {
-  return new PrismaClient({
+const adapter = new PrismaLibSQL(libsql);
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
-};
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db: PrismaClient = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
